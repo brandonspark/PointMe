ae030141f5f34be48713ead7975752c0b0000eb4
Brandon Wu <bjwu@andrew.cmu.edu>
2019-09-14 11:28:25 -0400

    modified:   Variable.py

--- Variable.py
+++ Variable.py
@@ -5,3 +5,5 @@ class Variable:
 		self.type = mtype
 		self.value = value
 		self.scope = scope # if variableScope > functionScope, this var should be deleted
+	def __repr__(self):
+		return 'Variable: Name: {}, Type: {}, Value: {}, Scope: {}'.format(self.name, self.type, self.value, self.scope)
