a6aa6b272e96c3bfe54540e47027f601556547ca
Brandon Wu <bjwu@andrew.cmu.edu>
2019-09-13 22:10:44 -0400

    modified:   main.py

--- main.py
+++ main.py
@@ -1,27 +1,63 @@
 import numpy as np
 
-functionTypes = ['int', 'string', 'char', 'bool', 'float', 'double', 'void', 'long']
+functionTypes = ['I', 'S', 'B', 'F', 'D', 'V', 'L']
 actualTypes = ['int', 'string', 'char', 'bool', 'float', 'double', 'long']
 typeMap = [('int', 'I'), ('string', 'S'), ('bool', 'B'), ('float', 'F'), ('double', 'D'), ('void', 'V'), ('long', 'L')]
 
 class Program():
 	def __init__(self, cfile):
-		totalText = self.cleanText(self.readText(cfile)) 
-		self.lines = self.splitLines(totalText)
-		self.funcDict = {}
-		for i, line in enumerate(self.lines):
-			flag, header = self.isFunctionHeader(line)
-			if flag:
-				self.funcDict[header] = i
-		for i, line in enumerate(self.lines):
+		"""
+		Initializes all the program data.
+		"""
+		self.lines = self.splitLines(self.cleanText(self.readText(cfile)))
+		self.shortenTypes()
+		self.funcDict = self.getFunctions()
+		self.scope = 0
+		print("hello", self.lines)
+		print("func", self.funcDict)
+	
+	def shortenTypes(self):
+		"""
+		Replaces all the types in the program with unique capital letters denoting the type.
+		"""
+
+		for i, line in enumerate(self.lines): #replaces all the types with shorter types
 			for type, ID in typeMap:
 				line = line.replace(type, ID)
 				self.lines[i] = line.replace(type, ID)
 			self.lines[i] = line.strip()
-		print("hello", self.lines)
-		print(self.funcDict)
-	
+
+	def getFunctions(self):
+		"""
+		This function will go to all the line numbers of all the functions in the program, and store only the lines
+		pertaining to that function in a list. It will then map the function's name to the list of the function's lines.
+		"""
+
+		functions = {}
+		for i, line in enumerate(self.lines): #gets all the function lines
+			flag, header = self.isFunctionHeader(line)
+			if flag:
+				functions[header] = i
+		for funcName in functions.keys():
+			scope = 0
+			goodLines = []
+			for line in self.lines[functions[funcName]:]:
+				goodLines.append(line)
+				if line == '{':
+					scope += 1
+				elif line == '}':
+					scope -= 1
+					if scope == 0:
+						break
+			functions[funcName] = goodLines
+		print(self.lines)
+		return functions
+
 	def readText(self, cfile):
+		"""
+		Compiles all the text in the program.
+		"""
+
 		f = open(cfile, 'r')
 		lines = f.readlines()
 		totalText = ''
@@ -30,12 +66,20 @@ class Program():
 		return totalText
 
 	def cleanText(self, totalText):
+		"""
+		Deals with endline things.
+		"""
+
 		totalText = totalText.replace('\n', '')
 		totalText = totalText.replace('\t', '')
 		totalText = totalText.replace('\\n', '\n')
 		return totalText
 
 	def splitLines(self, totalText):
+		"""
+		Splits the program by semicolon and curly braces.
+		"""
+
 		progLines = []
 		currentStr = ''
 		for char in totalText:
@@ -56,6 +100,10 @@ class Program():
 		return progLines
 
 	def isFunctionHeader(self, string):
+		"""
+		Returns true and the name of the function if a string is a function header.
+		"""
+
 		for type in functionTypes:
 			if type == string[:len(type)] and string[-1] == ')':
 				leftIndex = string.find('(')
