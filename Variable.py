ae2e2858b68d99876c3e6ce97e7aa2adec5c1636
Gabriel Chuang <chickenricecesium@gmail.com>
2019-09-14 01:36:33 -0400

    fixed variable

--- Variable.py
+++ Variable.py
@@ -1,6 +1,7 @@
 class Variable:
 
-	def __init__(self, type, value, scope):
-		self.type = type
+	def __init__(self, name, mtype, value, scope):
+		self.name = name
+		self.type = mtype
 		self.value = value
 		self.scope = scope # if variableScope > functionScope, this var should be deleted
